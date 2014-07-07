4deed04adeef52a370d2f8e7225dbb628e51ef08
Juan David Pastás Rivera <juandavid1707@gmail.com>
2014-07-06 19:18:47 -0500

    WIP paypal script.

--- public/curl.sh
+++ public/curl.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+real=$1
+
+payUrl(){
+  if [ -z $real ]; then
+    "https://svcs.paypal.com/AdaptivePayments/Pay"
+  else
+    "https://svcs.sandbox.paypal.com/AdaptivePayments/Pay"
+  fi
+}
+userId='AbVztxBLhT5zbJolEdTT3O8JRQzoZ0wqfjl6kDRkBCnYaIcYTfPaoqm4ECSJ'
+password='EBXxTBAcpVIC_eTC9brwvmYwLjctEJCp2f7UDGeSKvwzQHGU-k-5xTMik7zw'
+appId(){
+  if [ -z $real ]; then
+    ''
+  else
+    'APP-80W284485P519543T'
+  fi
+}
+data()
+  headers:
+    'X-PAYPAL-SECURITY-USERID':       @userId
+    'X-PAYPAL-SECURITY-PASSWORD':     @password
+    'X-PAYPAL-SECURITY-SIGNATURE':    '123'
+    'X-PAYPAL-REQUEST-DATA-FORMAT':   'JSON'
+    'X-PAYPAL-RESPONSE-DATA-FORMAT':  'JSON'
+    'X-PAYPAL-DEVICE-IPADDRESS':      '192.168.0.1'
+    'X-PAYPAL-APPLICATION-ID':        @appId()
+  data:
+    actionType: 'PAY'
+    returnUrl: 'none'
+    cancelUrl: 'none'
+    requestEnvelope:
+      errorLanguage: 'en_US'
+    currencyCode: 'USD'
+    receiverList:
+      receiver: options.receivers
+
+
 while read line ; do
   headers=(${headers[@]} -H "$line")
 done < public/headers.txt
